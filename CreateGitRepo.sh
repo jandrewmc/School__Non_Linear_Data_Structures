4c1492b0a1e85e5a5f0a835fb745cce4bc78e400
J Andrew McCormick <jandrewmc@gmail.com>
2016-05-27 13:55:07 -0400

    now in more general form

--- CreateGitRepo.sh
+++ CreateGitRepo.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ];
+if [ "$#" -ne 3 ];
 then
 	echo "Illegal Number of parameters"
-	echo "Please enter the name of the repo you wish to create"
+	echo "Please enter the user-name, visibility modifier, and repo name respectively"
+	exit
+fi
+if [ "$2" = '-private' ]
+then
+	curl -u "$1" https://api.github.com/user/repos -d '{"name":"'$3'", "public":"false"}'
 	exit
 fi
 
-curl -u 'jandrewmc' https://api.github.com/user/repos -d '{"name":"'$@'"}'
+if [ "$2" = '-public' ]
+then
+	curl -u "$1" https://api.github.com/user/repos -d '{"name":"'$3'", "public":"true"}'
+	exit
+fi
